1e2cf95dea07c730a11a8b71359ee639259d8eea
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-09-12 21:13:39 -0700

    Adding setup

---
+++ testing/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Dashboard from './Componenets/Dashboard';
+import Display from './Componenets/Display';
+
+function App() {
+  return (
+    <div>
+      <Display />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ testing/src/Componenets/Display.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Display({ strike }) {
+  return (
+    <div>
+      <h1>Balls</h1>
+      <h1>You clicked {strike}</h1>
+    </div>
+  )
+}
