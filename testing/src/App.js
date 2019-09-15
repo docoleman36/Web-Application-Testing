fd773ec33a551ef16b3acfdf5d507fe187ef0074
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-09-14 19:20:21 -0700

    Finished baseball logic

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Dashboard from './Componenets/Dashboard';
-import Display from './Componenets/Display';
+import Dashboard from './Components/Dashboard';
+import Display from './Components/Display';
 
 function App() {
+  const [balls, setBalls] = useState(0);
+  const [strikes, setStrikes] = useState(0);
+  const [fouls, setFouls] = useState(0);
+  const [hits, setHits] = useState(0);
+
   return (
-    <div>
-      <Display />
-      <Dashboard />
+    <div className="App">
+      <Display
+        balls={balls}
+        strikes={strikes}
+      />
+      <Dashboard
+        balls={balls}
+        strikes={strikes}
+        fouls={fouls}
+        hits={hits}
+        setBalls={setBalls}
+        setStrikes={setStrikes}
+        setFouls={setFouls}
+        setHits={setHits}
+      />
     </div>
   );
 }
