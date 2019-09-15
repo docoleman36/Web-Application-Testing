53da0e92516261b805d0f965fd27d5b33d7acdfc
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-09-14 20:32:26 -0700

    Working on tests

--- testing/src/App.test.js
+++ testing/src/App.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import * as rtl from '@testing-library/react'
+import { render } from "@testing-library/react";
+import { addBall, addStrike, addFoul, addHit } from './Components/Dashboard';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<App />);
 });
+
+test("addStrike works", () => {
+  expect(addStrike(0)).toBe(1);
+  expect(addStrike(2)).toBe(3);
+});
+
+test("addBall Works", () => {
+  expect(addBall(0)).toBe(1);
+  expect(addBall(2)).toBe(3);
+})
+
+test("addFoul Works", () => {
+  expect(addFoul(0)).toBe(1);
+  expect(addFoul(2)).toBe(2);
+})
+
+test("addHit Works", () => {
+  expect(addHit(0)).toBe(1);
+  expect(addHit(2)).toBe(3);
+})
